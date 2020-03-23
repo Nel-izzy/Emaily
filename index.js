bc691c9302a7d77d6f6d4c70aaf538f77f0dc01d
nelson <blackcodedigitals@gmail.com>
2020-03-23 15:16:23 +0100

    finshed OAuth set up

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express') 
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+require('./models/user')
+require('./services/passport')
+const authRoutes = require('./routes/authRoutes')
+const keys = require('./config/keys')
+
+
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology : true} )
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send({bye : 'Nelson'})
-})
+app.use(cookieSession({
+    maxAge : 30 * 24 * 3600 * 1000,
+    keys: [keys.cookieKey]
+}))
+
+app.use(passport.initialize())
+
+app.use(passport.session())
+
+authRoutes(app)
 
 const PORT = process.env.PORT || 5000
 
